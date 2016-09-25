65a03cb6f8bda92a1fa06ebbf2447d8003d9593f
mic <tomamic@ce.unipr.it>
2016-09-26 00:33:32 +0200

    backspace

--- js/slide-deck.js
+++ js/slide-deck.js
@@ -188,7 +188,7 @@ SlideDeck.prototype.onBodyKeyDown_ = function(e) {
       break;
 
     case 8: // Backspace
-      window.location.href='./index.html';
+      window.location='./index.html';
       e.preventDefault();
       break;
 
